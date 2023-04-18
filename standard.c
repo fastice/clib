b5ba2cb9a9827fbcde67f9810864aaac81dc9a41
Ian Joughin <irj@uw.edu>
2023-04-18 13:32:00 -0700

    updates for 64bit

--- standard.c
+++ standard.c
@@ -5,171 +5,186 @@
 #include <stdlib.h>
 /* #include <libkern/OSByteOrder.h> */
 #
-#define SWAP_SHORT(Var)  Var = *(short*)         SwapEndian((void*)&Var, sizeof(short))
-#define SWAP_USHORT(Var) Var = *(unsigned short*)SwapEndian((void*)&Var, sizeof(short))
-#define SWAP_LONG(Var)   Var = *(long*)          SwapEndian((void*)&Var, sizeof(long))
-#define SWAP_ULONG(Var)  Var = *(unsigned long*) SwapEndian((void*)&Var, sizeof(long))
-#define SWAP_RGB(Var)    Var = *(int*)           SwapEndian((void*)&Var, 3)
-#define SWAP_FLOAT(Var)  Var = *(float*)         SwapEndian((void*)&Var, sizeof(float))
-#define SWAP_DOUBLE(Var) Var = *(double*)        SwapEndian((void*)&Var, sizeof(double))
+#define SWAP_SHORT(Var) Var = *(int16_t *)SwapEndian((void *)&Var, sizeof(int16_t))
+#define SWAP_USHORT(Var) Var = *(uint16_t *)SwapEndian((void *)&Var, sizeof(int16_t))
+#define SWAP_LONG (Var) Var = *(int32_t *)SwapEndian((void *)&Var, sizeof(uint32_t))
+#define SWAP_ULONG(Var) Var = *(uint32_t *)SwapEndian((void *)&Var, sizeof(int32_t))
+#define SWAP_RGB(Var) Var = *(int32_t *)SwapEndian((void *)&Var, 3)
+#define SWAP_FLOAT(Var) Var = *(float *)SwapEndian((void *)&Var, sizeof(float))
+#define SWAP_DOUBLE(Var) Var = *(double *)SwapEndian((void *)&Var, sizeof(double))
 
-extern void *SwapEndian(void* Addr, const int Nb);
+extern void *SwapEndian(void *Addr, const int32_t Nb);
 static size_t byteSwap(void *ptr, size_t nitems, size_t size, int flags);
-/* 
+/*
    Useful procedures that can be used for all programs
 */
 
 /*
-  error -- prints error message with arguements "..." with format "fmt" 
+  error -- prints error message with arguements "..." with format "fmt"
   and then exits program. From K&R.
 */
 
-void error( char *fmt, ... )
+void error(char *fmt, ...)
 {
 	va_list args;
 
-	va_start(args,fmt);
-	fprintf(stderr,"error:  ");
-	vfprintf(stderr,fmt,args);
+	va_start(args, fmt);
+	fprintf(stderr, "error:  ");
+	vfprintf(stderr, fmt, args);
 	fprintf(stderr, "\n");
 	va_end(args);
 	exit(1);
 }
 
-void warning(FILE *fp, char *fmt, ... )
+void warning(FILE *fp, char *fmt, ...)
 {
 	va_list args;
 
-	va_start(args,fmt);
-	fprintf(stderr,"\nWarning:  ");
-	if(fp !=NULL) fprintf(fp,"\nWarning:  ");
-	vfprintf(stderr,fmt,args);
-	if(fp !=NULL) vfprintf(fp,fmt,args);
+	va_start(args, fmt);
+	fprintf(stderr, "\nWarning:  ");
+	if (fp != NULL)
+		fprintf(fp, "\nWarning:  ");
+	vfprintf(stderr, fmt, args);
+	if (fp != NULL)
+		vfprintf(fp, fmt, args);
 	fprintf(stderr, "\n");
-	if(fp !=NULL) fprintf(fp, "\n");
+	if (fp != NULL)
+		fprintf(fp, "\n");
 	va_end(args);
 }
 
-
 /* getline:  read a line, return length.  From K&R */
 
-int getlineobsolete(char *line, int imax)
+int32_t getlineobsolete(char *line, int32_t imax)
 {
-	if(fgets(line,imax,stdin) == NULL)
+	if (fgets(line, imax, stdin) == NULL)
 		return 0;
 	else
 		return strlen(line);
 }
- 
+
 /* fgetline:  read a line from a file, return length. */
 
-int fgetline(FILE *fp, char *line, int imax)
+int32_t fgetline(FILE *fp, char *line, int32_t imax)
 {
-	if( feof(fp) == EOF ) error("%s","fgetline -- EOF encountered");
-	if( ferror(fp) != 0 ) error("%s","fgetline -- file error");
+	if (feof(fp) == EOF)
+		error("%s", "fgetline -- EOF encountered");
+	if (ferror(fp) != 0)
+		error("%s", "fgetline -- file error");
 
-	if(fgets(line,imax,fp) == NULL) {
+	if (fgets(line, imax, fp) == NULL)
+	{
 		return 0;
-	}else {
+	}
+	else
+	{
 		return strlen(line);
 	}
 }
 
-
-/* 
-   Search through file for next occurence of BEGINDATA n, 
+/*
+   Search through file for next occurence of BEGINDATA n,
    where n = number of column. Return line count
 */
 
-int goToBeginningOfData(FILE *fp, int lineCount, int *ncolumns)
+int32_t goToBeginningOfData(FILE *fp, int32_t lineCount, int32_t *ncolumns)
 {
-	int lineLength;  
+	int32_t lineLength;
 	char line[LINEMAX];
 	char *string;
 
-	while( TRUE ) {  /* Loop to read lines */
- 
-		lineLength = fgetline(fp,line,LINEMAX); /* Read line */
+	while (TRUE)
+	{ /* Loop to read lines */
+
+		lineLength = fgetline(fp, line, LINEMAX); /* Read line */
 		lineCount++;
-		if(lineLength > 0) {
-			if( strchr(line,ENDDATA) != NULL ) /* Read data */
-				error("%s","goToBeginningOfData -- & with no matching # "); 
-			else if( (string = strchr(line,BEGINDATA) ) != NULL ) { 
-				if( sscanf((string+1),"%i",ncolumns) != 1) /*If BEGINDATA,get */
+		if (lineLength > 0)
+		{
+			if (strchr(line, ENDDATA) != NULL) /* Read data */
+				error("%s", "goToBeginningOfData -- & with no matching # ");
+			else if ((string = strchr(line, BEGINDATA)) != NULL)
+			{
+				if (sscanf((string + 1), "%i", ncolumns) != 1) /*If BEGINDATA,get */
 					error("%s",
-					      "goToBeginningOfData -- # found without n columns ");
-				return lineCount;   /* Found so exit */
-			}  
+						  "goToBeginningOfData -- # found without n columns ");
+				return lineCount; /* Found so exit */
+			}
 		}
 	}
 }
 
-
-/* 
+/*
    Read through file returning data strings.
    If end of data eod = TRUE else eod = FALSE.
 */
 
-int getDataString(FILE *fp, int lineCount, char *line,int *eod)
+int32_t getDataString(FILE *fp, int32_t lineCount, char *line, int32_t *eod)
 {
-	int lineLength;  
-	int count;
+	int32_t lineLength;
+	int32_t count;
 	count = 0;
-	while( TRUE ) {  /* Loop to read lines */
- 
-		lineLength = fgetline(fp,line,LINEMAX); /* Read line */
+	while (TRUE)
+	{ /* Loop to read lines */
+
+		lineLength = fgetline(fp, line, LINEMAX); /* Read line */
 		lineCount++;
 		count++;
-		if( strchr(line,COMMENT) == NULL &&   
-		    strpbrk(line,
-			    "0123456789.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
-		    != NULL ) {
+		if (strchr(line, COMMENT) == NULL &&
+			strpbrk(line,
+					"0123456789.ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") != NULL)
+		{
 			*eod = FALSE;
-			return lineCount; /* Data */ 
+			return lineCount; /* Data */
 		}
-         
-		if( strchr(line,ENDDATA) != NULL &&
-		    strchr(line,COMMENT) == NULL ) { /* End of data */
+
+		if (strchr(line, ENDDATA) != NULL &&
+			strchr(line, COMMENT) == NULL)
+		{ /* End of data */
 			*eod = TRUE;
 			return lineCount;
 		}
-		if( count >= 100000) error("getDataString: Missing end of data");
+		if (count >= 100000)
+			error("getDataString: Missing end of data");
 	}
 }
 
-
 /* Search through file for next occurence of ENDDATA */
 
-int goToEndOfData(FILE *fp, int lineCount, int *ndata)
+int32_t goToEndOfData(FILE *fp, int32_t lineCount, int32_t *ndata)
 {
-	int lineLength;  
+	int32_t lineLength;
 	char line[LINEMAX];
 
-	while( TRUE ) {  /* Loop to read lines */
- 
-		lineLength = fgetline(fp,line,LINEMAX); /* Read line */
-		lineCount++;
-		if( strchr(line,COMMENT) == NULL &&   
-		    strpbrk(line,"0123456789.EDed") != NULL ) (*ndata)++; /*Count data*/
+	while (TRUE)
+	{ /* Loop to read lines */
 
-		if( strchr(line,ENDDATA) != NULL ) return lineCount;
+		lineLength = fgetline(fp, line, LINEMAX); /* Read line */
+		lineCount++;
+		if (strchr(line, COMMENT) == NULL &&
+			strpbrk(line, "0123456789.EDed") != NULL)
+			(*ndata)++; /*Count data*/
+
+		if (strchr(line, ENDDATA) != NULL)
+			return lineCount;
 	}
 }
 
 /* Search through file for next occurence of ENDDATA */
 
-int goToNextStringOccurence(FILE *fp, int lineCount, char *string,char *line)
+int32_t goToNextStringOccurence(FILE *fp, int32_t lineCount, char *string, char *line)
 {
-	int lineLength;  
+	int32_t lineLength;
 
-	while( TRUE ) {  /* Loop to read lines */
-		lineLength = fgetline(fp,line,LINEMAX); /* Read line */
+	while (TRUE)
+	{											  /* Loop to read lines */
+		lineLength = fgetline(fp, line, LINEMAX); /* Read line */
 		lineCount++;
-		if(feof(fp) != 0) return -1;
-		if( strstr(line,string) != NULL) return lineCount;
+		if (feof(fp) != 0)
+			return -1;
+		if (strstr(line, string) != NULL)
+			return lineCount;
 	}
 }
-
 
 /* Open file for input, exit if error occurs */
 
@@ -177,102 +192,120 @@ FILE *openInputFile(char *filename)
 {
 	FILE *fp;
 
-	if( (fp = fopen(filename,"r")) == NULL )  /* Error if unable to open file */
-		error("\n\n%s |%s|\n","openInputFile -- Unable to open file: ",filename);
+	if ((fp = fopen(filename, "r")) == NULL) /* Error if unable to open file */
+		error("\n\n%s |%s|\n", "openInputFile -- Unable to open file: ", filename);
 	return fp;
 }
 
-
-size_t byteSwap(void *ptr, size_t nitems, size_t size, int flags) {
-	long long i;	
-	short int *ptr16;
-	unsigned long *ptrLong;
+size_t byteSwap(void *ptr, size_t nitems, size_t size, int flags)
+{
+	int64_t i;
+	int16_t *ptr16;
+	uint32_t *ptrLong;
 	double *ptrDouble;
 	size_t n;
 	/*
-	  No need to swap byte data 
+	  No need to swap byte data
 	*/
-	switch(flags) {
-	case BYTEFLAG :
-		n = (size_t) nitems;
+	switch (flags)
+	{
+	case BYTEFLAG:
+		n = (size_t)nitems;
 		break;
-	case FLOAT32FLAG :
-		n=(nitems*size)/sizeof(float);
-		ptrLong = (unsigned long *)ptr;
-		for(i = 0; i < n; i++) ptrLong[i] = SWAP_ULONG(ptrLong[i]);
-		break;		
-	case FLOAT64FLAG :
-		n = (nitems*size)/sizeof(double);
+	case FLOAT32FLAG:
+		n = (nitems * size) / sizeof(float);
+		ptrLong = (uint32_t *)ptr;
+		for (i = 0; i < n; i++)
+			ptrLong[i] = SWAP_ULONG(ptrLong[i]);
+		break;
+	case FLOAT64FLAG:
+		n = (nitems * size) / sizeof(double);
 		ptrDouble = (double *)ptr;
-		for(i = 0; i < n; i++) ptrDouble[i] = SWAP_DOUBLE(ptrDouble[i]);
-		break;		
-	case INT32FLAG :
-		n = (nitems*size)/sizeof(long int);
-		ptrLong = (unsigned long *)ptr;
-		for(i = 0; i < n; i++) ptrLong[i] = SWAP_ULONG(ptrLong[i]);
-		break;		
-	case INT16FLAG :
-		n = (nitems*size)/sizeof(short int);
-		ptr16 = (short int *)ptr;
-		for(i = 0; i < n; i++) ptr16[i] = SWAP_USHORT(ptr16[i]);
-		break;		
-	case INT16FLAGNOSWAP :
-		n = (nitems*size)/sizeof(short int);
-		break;		
+		for (i = 0; i < n; i++)
+			ptrDouble[i] = SWAP_DOUBLE(ptrDouble[i]);
+		break;
+	case INT32FLAG:
+		n = (nitems * size) / sizeof(int32_t);
+		ptrLong = (uint32_t *)ptr;
+		for (i = 0; i < n; i++)
+			ptrLong[i] = SWAP_ULONG(ptrLong[i]);
+		break;
+	case INT16FLAG:
+		n = (nitems * size) / sizeof(int16_t);
+		ptr16 = (int16_t *)ptr;
+		for (i = 0; i < n; i++)
+			ptr16[i] = SWAP_USHORT(ptr16[i]);
+		break;
+	case INT16FLAGNOSWAP:
+		n = (nitems * size) / sizeof(int16_t);
+		break;
 	}
-	return((size_t) n);	
-}	
+	return ((size_t)n);
+}
 
 /*
   Read raw data and byteswap if needed
-*/ 
-size_t freadBS(void *ptr, size_t nitems, size_t size, FILE *fp,int flags)
+*/
+size_t freadBS(void *ptr, size_t nitems, size_t size, FILE *fp, int32_t flags)
 {
-	int swap;
+	int32_t swap;
 	size_t retVal;
 	/* Use fread to read the data as requested */
-	retVal = fread(ptr,nitems,size,fp);
+	retVal = fread(ptr, nitems, size, fp);
 #ifdef i386
 	swap = TRUE;
 #endif
 #ifdef x86_64
 	swap = TRUE;
 #endif
-	if(swap == TRUE) byteSwap(ptr, nitems, size, flags);
-	else error("No current noSwap architecture defined  - code needs update\n");
+
+#ifdef arm64
+	swap = TRUE;
+#endif
+	if (swap == TRUE)
+		byteSwap(ptr, nitems, size, flags);
+	else
+		error("No current noSwap architecture defined  - code needs update\n");
 	return retVal;
 }
 /*
   Read raw data and byteswap if needed
-*/ 
-size_t fwriteBS(void *ptr, size_t nitems, size_t size, FILE *fp,int flags)
+*/
+size_t fwriteBS(void *ptr, size_t nitems, size_t size, FILE *fp, int32_t flags)
 {
 	size_t retVal;
-	int swap;
+	int32_t swap;
 	/*
 	  ++++++++++++++++++++++++++++++start i386++++++++++++++++++++++++++++++++++++++++
 	*/
-	if(flags != FLOAT32FLAG && flags != FLOAT64FLAG && flags != INT32FLAG && flags != INT16FLAG && flags != BYTEFLAG) error("fwriteBS: invalid flag");	
+	if (flags != FLOAT32FLAG && flags != FLOAT64FLAG && flags != INT32FLAG && flags != INT16FLAG && flags != BYTEFLAG)
+		error("fwriteBS: invalid flag");
 	swap = FALSE;
 #ifdef i386
-	swap=TRUE;
+	swap = TRUE;
 #endif
 #ifdef x86_64
-	swap=TRUE;
-#endif 
-	if(swap == FALSE) error("No current noSwap architecture defined  - code needs update\n");
-	if(swap == TRUE)  byteSwap(ptr, nitems, size, flags);
-	retVal=fwrite(ptr,nitems,size,fp);
+	swap = TRUE;
+#endif
+#ifdef arm64
+	swap = TRUE;
+#endif
+	if (swap == FALSE)
+		error("No current noSwap architecture defined  - code needs update\n");
+	if (swap == TRUE)
+		byteSwap(ptr, nitems, size, flags);
+	retVal = fwrite(ptr, nitems, size, fp);
 	/* Swap back */
-	if(swap == TRUE)  byteSwap(ptr, nitems, size, flags);
-	return(retVal);
+	if (swap == TRUE)
+		byteSwap(ptr, nitems, size, flags);
+	return (retVal);
 }
 
+static int32_t _TestEndian = 1;
 
-static long _TestEndian=1;
-
-int IsLittleEndian(void) {
-	return *(char*)&_TestEndian;
+int32_t IsLittleEndian(void)
+{
+	return *(char *)&_TestEndian;
 }
 
 /******************************************************************************
@@ -281,49 +314,54 @@ int IsLittleEndian(void) {
   EXAMPLE: float F=123.456;; SWAP_FLOAT(F);
 ******************************************************************************/
 
-void *SwapEndian(void* Addr, const int Nb) {
+void *SwapEndian(void *Addr, const int32_t Nb)
+{
 	static char Swapped[16];
-	switch (Nb) {
-	case 2:	Swapped[0]=*((char*)Addr+1);
-		Swapped[1]=*((char*)Addr  );
+	switch (Nb)
+	{
+	case 2:
+		Swapped[0] = *((char *)Addr + 1);
+		Swapped[1] = *((char *)Addr);
 		break;
-	case 3:	// As far as I know, 3 is used only with RGB images
-		Swapped[0]=*((char*)Addr+2);
-		Swapped[1]=*((char*)Addr+1);
-		Swapped[2]=*((char*)Addr  );
+	case 3: // As far as I know, 3 is used only with RGB images
+		Swapped[0] = *((char *)Addr + 2);
+		Swapped[1] = *((char *)Addr + 1);
+		Swapped[2] = *((char *)Addr);
 		break;
-	case 4:	Swapped[0]=*((char*)Addr+3);
-		Swapped[1]=*((char*)Addr+2);
-		Swapped[2]=*((char*)Addr+1);
-		Swapped[3]=*((char*)Addr  );
+	case 4:
+		Swapped[0] = *((char *)Addr + 3);
+		Swapped[1] = *((char *)Addr + 2);
+		Swapped[2] = *((char *)Addr + 1);
+		Swapped[3] = *((char *)Addr);
 		break;
-	case 8:	Swapped[0]=*((char*)Addr+7);
-		Swapped[1]=*((char*)Addr+6);
-		Swapped[2]=*((char*)Addr+5);
-		Swapped[3]=*((char*)Addr+4);
-		Swapped[4]=*((char*)Addr+3);
-		Swapped[5]=*((char*)Addr+2);
-		Swapped[6]=*((char*)Addr+1);
-		Swapped[7]=*((char*)Addr  );
+	case 8:
+		Swapped[0] = *((char *)Addr + 7);
+		Swapped[1] = *((char *)Addr + 6);
+		Swapped[2] = *((char *)Addr + 5);
+		Swapped[3] = *((char *)Addr + 4);
+		Swapped[4] = *((char *)Addr + 3);
+		Swapped[5] = *((char *)Addr + 2);
+		Swapped[6] = *((char *)Addr + 1);
+		Swapped[7] = *((char *)Addr);
 		break;
-	case 16:Swapped[0]=*((char*)Addr+15);
-		Swapped[1]=*((char*)Addr+14);
-		Swapped[2]=*((char*)Addr+13);
-		Swapped[3]=*((char*)Addr+12);
-		Swapped[4]=*((char*)Addr+11);
-		Swapped[5]=*((char*)Addr+10);
-		Swapped[6]=*((char*)Addr+9);
-		Swapped[7]=*((char*)Addr+8);
-		Swapped[8]=*((char*)Addr+7);
-		Swapped[9]=*((char*)Addr+6);
-		Swapped[10]=*((char*)Addr+5);
-		Swapped[11]=*((char*)Addr+4);
-		Swapped[12]=*((char*)Addr+3);
-		Swapped[13]=*((char*)Addr+2);
-		Swapped[14]=*((char*)Addr+1);
-		Swapped[15]=*((char*)Addr  );
+	case 16:
+		Swapped[0] = *((char *)Addr + 15);
+		Swapped[1] = *((char *)Addr + 14);
+		Swapped[2] = *((char *)Addr + 13);
+		Swapped[3] = *((char *)Addr + 12);
+		Swapped[4] = *((char *)Addr + 11);
+		Swapped[5] = *((char *)Addr + 10);
+		Swapped[6] = *((char *)Addr + 9);
+		Swapped[7] = *((char *)Addr + 8);
+		Swapped[8] = *((char *)Addr + 7);
+		Swapped[9] = *((char *)Addr + 6);
+		Swapped[10] = *((char *)Addr + 5);
+		Swapped[11] = *((char *)Addr + 4);
+		Swapped[12] = *((char *)Addr + 3);
+		Swapped[13] = *((char *)Addr + 2);
+		Swapped[14] = *((char *)Addr + 1);
+		Swapped[15] = *((char *)Addr);
 		break;
 	}
-	return (void*)Swapped;
+	return (void *)Swapped;
 }
-
--- standard.h
+++ standard.h
@@ -23,51 +23,48 @@
 #define CLIGHT 2.99792458e8
 #define E0 8.841941e-12
 #define U0 1.256637e-6
-#define max(A,B) ((A) > (B) ? (A) : (B))
-#define min(A,B) ((A) < (B) ? (A) : (B))
+#define max(A, B) ((A) > (B) ? (A) : (B))
+#define min(A, B) ((A) < (B) ? (A) : (B))
 /*
     Include file for library of standard routines
 */
 
-
-
 /* Print error and exit */
-   void error( char *fmt, ... );     
+void error(char *fmt, ...);
 
 /* Print warning and write to log file if *fp !=NULL */
-   void warning( FILE *fp,char *fmt, ... );     
+void warning(FILE *fp, char *fmt, ...);
 
 /* getline: read a line, return length */
-   int getlineobsolete(char *line, int imax);  
+int32_t getlineobsolete(char *line, int32_t imax);
 
 /* fgetline: read a line from file, return length */
-   int fgetline(FILE *fp, char *line, int imax);  
+int32_t fgetline(FILE *fp, char *line, int32_t imax);
 
-/* 
-   Search through file for next occurence of BEGINNINGOFDATA n, 
+/*
+   Search through file for next occurence of BEGINNINGOFDATA n,
    where n = number of columns. Return line count.
 */
 
-   int goToBeginningOfData(FILE *fp, int lineCount, int *ncolumns);
-
+int32_t goToBeginningOfData(FILE *fp, int32_t lineCount, int32_t *ncolumns);
 
 /* Search through file for next occurence of ENDOFDATA */
 
-   int goToEndOfData(FILE *fp, int lineCount, int *ndata);
+int32_t goToEndOfData(FILE *fp, int32_t lineCount, int32_t *ndata);
 
 /* Open file for input, exit if error occurs */
-   FILE *openInputFile(char *filename);
-/* 
+FILE *openInputFile(char *filename);
+/*
    Go to next occurence of "string" in file, return negative if EOF.
 */
- int goToNextStringOccurence(FILE *fp, int lineCount,char *string,char *line);
-/* 
+int32_t goToNextStringOccurence(FILE *fp, int32_t lineCount, char *string, char *line);
+/*
   Read through file returning data strings.
   If end of data eod = TRUE else eod = FALSE.
 */
-   int getDataString(FILE *fp, int lineCount, char *line,int *eod);
+int32_t getDataString(FILE *fp, int32_t lineCount, char *line, int32_t *eod);
 /*
   Read/write raw data and byteswap if needed
-*/ 
-  size_t freadBS(void * ptr, size_t nitems, size_t size, FILE *fp,int flags);
-  size_t fwriteBS(void * ptr, size_t nitems, size_t size, FILE *fp,int flags);
+*/
+size_t freadBS(void *ptr, size_t nitems, size_t size, FILE *fp, int32_t flags);
+size_t fwriteBS(void *ptr, size_t nitems, size_t size, FILE *fp, int32_t flags);
